7250fa418bce8a8f68deb204b0d2aaebcdf2b4ad
FreytagDom <dominicfreytag@web.de>
2023-04-23 21:53:25 +0200

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://dominicfreytag.de",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
