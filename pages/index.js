0ccc57c09ecbb199da0fd5f01feb57475b25d515
FreytagDom <dominicfreytag@web.de>
2023-04-23 21:32:47 +0200

    more

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Dominic Freytag Poretfolio</title>
+        <title>Dominic Freytag|Portfolio|Web-Developer|WebApp</title>
 
         <meta
           name="description"
-          content="Auf dieser Seite erzähle ich ein wenig zu meiner Person und meinen Freizeitprojekten als WebApp-Developer, die Projekte sind verlinkt.  "
+          content="Auf dieser Seite erzähle ich ein wenig zu meiner Person und meinen Freizeitprojekten als WebApp-Developer, die Projekte sind verlinkt. Vom Elektroniker zum WebApp-Developer"
         />
         <link rel="icon" type="image/png" sizes="32x32" href="/DF3.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/DF3.png" />
