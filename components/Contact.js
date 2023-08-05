7b5c00913817c16caff85857b86b1f74bf4fd981
FreytagDom <dominicfreytag@web.de>
2023-08-05 18:10:55 +0200

    add email icon and addres

--- components/Contact.js
+++ components/Contact.js
@@ -3,9 +3,14 @@ import Image from "next/image";
 import Github from '../public/Github.svg'
 import Linkedin from '../public/Linkedin.svg'
 import XingLogo from '../public/XingLogo.png'
+import EmailSend from '../public/EmailSend.png'
 import Link from "next/link";
 
+
 export default function Contact() {
+    const handleEmailButtonClick = () => {
+        window.location.href = "mailto:dominic.freytag@dominicfreytag.de";
+      };
 return(
     <>
     <ContacWrapper>
@@ -18,7 +23,9 @@ return(
         <Link href='https://www.xing.com/profile/Dominic_Freytag/'>
         <Xing   src={XingLogo} alt="Xing"></Xing>
         </Link>
-    <Email>dominicfreytag@web.de</Email>
+    <EmailButton onClick={handleEmailButtonClick}  >
+<Email src={EmailSend} alt="Email send"></Email>
+    </EmailButton>
     </ContacWrapper>
     </>
 )
@@ -26,36 +33,47 @@ return(
 }
 
 const ContacWrapper = styled.div`
-display: grid;
- grid-template-columns: repeat(5, 1fr);
- max-width: 15rem;
+ display: grid;
+ grid-template-columns: repeat(5, 2fr);
+ max-width: 25rem;
  align-content: center;
  padding-left: 2rem;
+ align-items: center;
+ justify-items: center;
  `;
 
-const Email = styled.div`
-padding-left: 3rem;
-color: aquamarine;
-font-size: 1rem;
-color: aliceblue;
-z-index: 3;
+const EmailButton = styled.button`
+ background: transparent;
+ border: none;
+ outline : none!important;
+ cursor: pointer ;
+`;
+
+const Email =styled(Image)`
+ z-index: 3;
+ width: 1.5rem;
+ height:  1.5rem;
+ margin-left: 0.5rem;
 `;
 
 const GitHub = styled(Image)`
-z-index: 3;
-width: 1.5rem;
-height:  1.5rem;
-text-decoration: none;
+ z-index: 3;
+ width: 1.5rem;
+ height:  1.5rem;
+ text-decoration: none;
+ margin-left: 1rem;
 `;
 
 const LinkedIn = styled(Image)`
-z-index: 3;
-width: 1.5rem;
-height:  1.5rem;
+ z-index: 3;
+ width: 1.5rem;
+ height:  1.5rem;
+ margin-left: 1rem;
 `;
 
 const Xing = styled(Image)`
-z-index: 3;
-width: 1.4rem;
-height:  1.4rem;
+ z-index: 3;
+ width: 1.4rem;
+ height:  1.4rem;
+ margin-left: 1rem;
 `;
